7740a19570e84eefc08beff65f87ffc8073bb895
ZeroxTV <leonbaronick@gmail.com>
2017-01-18 17:32:02 +0100

    Added ZItem Library and dependency for ZCore

---
+++ src/main/java/de/zeroxtv/zconomy/ZObjects/ZItem.java
@@ -0,0 +1,54 @@
+package de.zeroxtv.zconomy.ZObjects;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ZeroxTV
+ */
+public class ZItem {
+    private static ArrayList<ZItem> zItems = new ArrayList<>();
+    public Double value;
+    public short damage;
+    private Material material;
+
+    public Double offerValueMin;
+    public Double offerValueMax;
+    public Double requestValueMin;
+    public Double requestValueMax;
+
+    public ZItem(Material material, short damage, Double value, Double offerValueMin, Double offerValueMax, Double requestValueMin, Double requestValueMax) {
+        this.value = value;
+        this.material = material;
+        this.damage = damage;
+        this.offerValueMin = offerValueMin;
+        this.offerValueMax = offerValueMax;
+        this.requestValueMin = requestValueMin;
+        this.requestValueMax = requestValueMax;
+        zItems.add(this);
+    }
+
+    public ItemStack getItem() {
+        ItemStack item;
+        if (damage == -1) {
+            item = new ItemStack(material, 1);
+        } else {
+            item = new ItemStack(material, 1, damage);
+        }
+        return item;
+    }
+
+    public static ArrayList<ZItem> getItems() {
+        return zItems;
+    }
+
+    public boolean hasOffer() {
+        return offerValueMin != 0 && offerValueMax != 0;
+    }
+
+    public boolean hasRequest() {
+        return requestValueMin != 0 && requestValueMax != 0;
+    }
+}
---
+++ src/main/java/de/zeroxtv/zconomy/ZObjects/ZItemLoader.java
@@ -0,0 +1,64 @@
+package de.zeroxtv.zconomy.ZObjects;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by ZeroxTV
+ */
+public class ZItemLoader {
+
+    public static ArrayList<ZItem> loadItem(File configFile) {
+        try {
+            YamlConfiguration config = new YamlConfiguration();
+            config.load(configFile);
+
+            for (String path : config.getKeys(false)) {
+                new ZItem(Material.getMaterial(config.getString(path + ".material")),
+                        Short.parseShort(config.getString(path + ".durability")),
+                        config.getDouble(path + ".value"),
+                        config.getDouble(path + ".offer.valueMin"),
+                        config.getDouble(path + ".offer.valueMax"),
+                        config.getDouble(path + ".request.valueMin"),
+                        config.getDouble(path + ".request.valueMax"));
+            }
+
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        return ZItem.getItems();
+    }
+
+    public static ArrayList<ZItem> createItem(File configFile, ItemStack itemStack, Double value,Double offerValueMin, Double offerValueMax, Double requestValueMin, Double requestValueMax) {
+        try {
+            YamlConfiguration config = new YamlConfiguration();
+            config.load(configFile);
+            config.options().copyDefaults(true);
+            String material = itemStack.getType().name().toUpperCase();
+            String path;
+            for (int i = 0; i < 100; i++) {
+                if (config.contains(material + "_" + i)) {
+                    path = material + "_" + i;
+                    config.addDefault(path + ".material", itemStack.getType().name());
+                    config.addDefault(path + ".durability", itemStack.getDurability());
+                    config.addDefault(path + ".value", value);
+                    config.addDefault(path + ".offer.minValue", offerValueMin);
+                    config.addDefault(path + ".offer.maxValue", offerValueMax);
+                    config.addDefault(path + ".request.minValue", requestValueMin);
+                    config.addDefault(path + ".request.maxValue", requestValueMax);
+                    config.save(configFile);
+                    new ZItem(itemStack.getType(), itemStack.getDurability(), value, offerValueMin, offerValueMax, requestValueMin, requestValueMax);
+                }
+            }
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        return ZItem.getItems();
+    }
+}
